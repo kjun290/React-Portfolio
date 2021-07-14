1dbbade83e465bc1bfe1cf72eb77ae29b133c995
Kevin Jun <kwjun90@gmail.com>
2021-07-14 16:06:52 -0500

    atted main.js in pages and file structure

---
+++ reactportfolio/src/pages/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Main() {
+    return (
+        <div>
+            <p className='text-center fs-4 p-5'>
+                Recented completed a fullstack coding bootcamp from Northwestern University
+            </p>
+        </div>
+    );
+}
+
+export default Main;
